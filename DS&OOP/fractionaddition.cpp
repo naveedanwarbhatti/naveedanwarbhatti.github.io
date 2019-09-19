27f8ffd580f35ed9037ddd9e92e9f52509a1686d
Naveed Bhatti <hunarame@gmail.com>
2019-09-19 09:21:34 +0200

    Update fractionaddition.cpp

--- DS&OOP/fractionaddition.cpp
+++ DS&OOP/fractionaddition.cpp
@@ -11,8 +11,8 @@ struct Fraction
 Fraction add(Fraction a, Fraction b)
 {
 	Fraction temp;
-	temp.numerator = a.numerator*b.denominator + a.numerator*b.denominator;
-	temp.denominator = a.denominator * b.denominator;
+	temp.numerator = (a.numerator * b.denominator) + (a.numerator * b.denominator);
+	temp.denominator = (a.denominator * b.denominator);
 	return temp;
 }
 
